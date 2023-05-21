10e6811674b28d2eb25387de32663ab02610e079
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-05-21 12:46:54 +0800

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="flex-col w-full flex-center">
+      <h1 className="text-center head_text">Discover & Share</h1>
+    </section>
+  );
+};
+
+export default Home;
