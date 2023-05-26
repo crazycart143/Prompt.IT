f2127b8c40ebb4311a55fd9a3f68fdd5803e869a
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-05-26 23:10:08 +0800

    added mongodb and mongoose modules

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -51,7 +51,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -82,7 +82,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
