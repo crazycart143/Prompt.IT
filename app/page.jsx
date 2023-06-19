fdf6e2a15ed8ad7519578c3d625041feb33def7e
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-06-19 21:10:52 +0800

    latest update

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,10 @@ const Home = () => {
       <h1 className="text-center head_text">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+        <span className="text-center orange_gradient"> AI-Powered Prompts</span>
       </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI programming tool for modern world to
+      <p className="text-center desc">
+        Prompt.IT is an open-source AI programming tool for modern world to
         discover, create and share creative prompts
       </p>
       <Feed />
