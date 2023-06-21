47e97e8fffaf85d628c5d7d89b7cda0c80f2fa96
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-06-21 10:35:02 +0800

    added delete notifications when prompt is deleted

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,4 +1,5 @@
 import Prompt from "@models/prompt";
+import Notification from "@models/notification";
 import { connectToDb } from "@utils/database";
 
 export const GET = async (request, { params }) => {
@@ -47,7 +48,9 @@ export const DELETE = async (request, { params }) => {
     await connectToDb();
 
     // Find the prompt by ID and remove it
-    await Prompt.findByIdAndRemove(params.id);
+    const deletedPrompt = await Prompt.findByIdAndRemove(params.id);
+
+    await Notification.deleteMany({ recipient: deletedPrompt.creator });
 
     return new Response("Prompt deleted successfully", { status: 200 });
   } catch (error) {
